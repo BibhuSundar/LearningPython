8afc6109ba155dfc8ff143dd2ccb1354ca652175
abbcreation <bibhu38.das@gmail.com>
2026-09-01 17:59:22 +0530

    Add lab exercises for Python basics and variables

---
+++ src/ex_02_Keywords_Identifier_Variables/Lab005_Variable_Part1.py
@@ -0,0 +1,11 @@
+age = 65
+age  = "Sundar"
+# Dynamic Typed Language. - It can understand you are doing, want.
+pi = 3.14
+name = "BIBHU"
+name = "Bibhu"
+name = "Bibhu"
+name = "Bibhu"
+name = "Bibhu"
+name = "Das"
+print(name)
---
+++ src/ex_02_Keywords_Identifier_Variables/Lab006_Identifier.py
@@ -0,0 +1,15 @@
+#Variable is nothing but called an identifier and the value is called a literal.
+
+age = 65
+_age = 65
+print(age)
+print(_age)
+
+_ = 12
+print(_)
+_ = _+1
+print(_)
+
+#123abc = 90, # Not allowed. 
+abc123 = "abc123"
+print(abc123)
